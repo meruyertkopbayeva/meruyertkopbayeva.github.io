544e439d920200b1d57d1e04a6e53b3f624ed2e6
meruyertkopbayeva <meruyert.kopbayeva@nu.edu.kz>
2023-04-12 23:16:10 +0600

    fix 2

--- cert.js
+++ cert.js
@@ -1,38 +1,14 @@
 var certID = 0;
 
 $(document).ready(function () {
-    console.log(window.outerWidth)
-    function createCert(cert_id) {
-        let bp = $('<div>',{
-            class: 'flex-column'
-        })
-        let bc = $('<div>',{
-            class: 'flex-column'
-        })
-        let imgCert = $('<img>',{
-            id: "cert" + cert_id,
-            class: "display-cert",
-            src: "certificates/cert" + cert_id + ".jpg",
-            height: "500px"
-        })
-        bc.append(imgCert)
-        let a = $('<a>',{
-            href: "certificates/cert" + cert_id + ".pdf",
-            download: "cert" + cert_id + ".pdf",
-            class: "download"
-        })
-        a.append('<img class="download-btn" src="images/download.png" height="40px" width="40px" alt="">')
-        bc.append(a)
-        bp.append(bc)
-        return bp
-    }
     for (let i = 1; i < 10; i++) {
-        $('.flex-container').append('<img onclick="document.location = \'#cert' + i +'\'" class="flex-item" src="certificates/cert' + i + '.jpg" height="500px" srcset="">')
+        $('.flex-container').append('<img class="flex-item" src="certificates/cert' + i + '.jpg" height="500px" srcset="">')
         $('.selected-certs').append('<img is_selected="false" cert_id="' + (i - 1) +'" class="cert" src="certificates/cert' + i + '.jpg" height="50px" srcset="">')
-        $('.certificates-all').append(createCert(i))
     }
     $('.flex-item')[certID].style.display ='block'
     $('.cert:eq(0)').attr('is_selected','true')
+    $('.download').attr('href','certificates/cert' + 1 + '.pdf')
+    $('.download').attr('download','cert' + 1 + '.pdf')
     $('.hamburger').click(function (){
         $('#hamburger-input1').prop('checked',!$('#hamburger-input1').prop('checked'))
         $('#hamburger-input2').prop('checked',!$('#hamburger-input2').prop('checked'))
@@ -44,6 +20,8 @@ $(document).ready(function () {
         if (certID > $('.flex-item').length - 1) certID = 0
         $('.flex-item')[certID].style.display ='block';
         $('.cert:eq(' + certID + ')').attr('is_selected','true');
+        $('.download').attr('href','certificates/cert' + (certID + 1) + '.pdf')
+        $('.download').attr('download','cert' + (certID + 1) + '.pdf')
     });
     $('#left-click').click(function(self) {
         $('.flex-item')[certID].style.display ='none';
@@ -53,14 +31,17 @@ $(document).ready(function () {
         if (certID > $('.flex-item').length - 1) certID = 0;
         $('.flex-item')[certID].style.display ='block';
         $('.cert:eq(' + certID + ')').attr('is_selected','true');
+        $('.download').attr('href','certificates/cert' + (certID + 1) + '.pdf')
+        $('.download').attr('download','cert' + (certID + 1) + '.pdf')
     });
-
     $('.cert').click(function() {
         $('.flex-item')[certID].style.display ='none';
         $('.cert:eq(' + certID + ')').attr('is_selected','false');
-        certID = this.getAttribute('cert_id')
+        certID = parseInt(this.getAttribute('cert_id'))
         $('.flex-item')[certID].style.display ='block';
         $('.cert:eq(' + certID + ')').attr('is_selected','true');
+        $('.download').attr('href','certificates/cert' + (certID + 1) + '.pdf')
+        $('.download').attr('download','cert' + (certID + 1) + '.pdf')
     });
 
 
