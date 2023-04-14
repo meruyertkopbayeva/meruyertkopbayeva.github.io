120ed3c49d93b12a29ee82d4cc05bd72f87094ae
meruyertkopbayeva <meruyert.kopbayeva@nu.edu.kz>
2023-04-14 23:20:55 +0600

    fix 5

--- cert.js
+++ cert.js
@@ -2,12 +2,12 @@ var certID = 0;
 
 $(document).ready(function () {
     for (let i = 1; i < 10; i++) {
-        $('.flex-container').append('<img class="flex-item" src="certificates/cert' + i + '.jpg" height="500px" srcset="">')
-        $('.selected-certs').append('<img is_selected="false" cert_id="' + (i - 1) +'" class="cert" src="certificates/cert' + i + '.jpg" height="50px" srcset="">')
+        $('.flex-container').append('<img class="flex-item" src="/resources/cert' + i + '.jpg" height="500px" srcset="">')
+        $('.selected-certs').append('<img is_selected="false" cert_id="' + (i - 1) +'" class="cert" src="/resources/cert' + i + '.jpg" height="50px" srcset="">')
     }
     $('.flex-item')[certID].style.display ='block'
     $('.cert:eq(0)').attr('is_selected','true')
-    $('.download').attr('href','certificates/cert' + 1 + '.pdf')
+    $('.download').attr('href','/resources/cert' + 1 + '.pdf')
     $('.download').attr('download','cert' + 1 + '.pdf')
     $('.hamburger').click(function (){
         $('#hamburger-input1').prop('checked',!$('#hamburger-input1').prop('checked'))
@@ -20,7 +20,7 @@ $(document).ready(function () {
         if (certID > $('.flex-item').length - 1) certID = 0
         $('.flex-item')[certID].style.display ='block';
         $('.cert:eq(' + certID + ')').attr('is_selected','true');
-        $('.download').attr('href','certificates/cert' + (certID + 1) + '.pdf')
+        $('.download').attr('href','/resources/cert' + (certID + 1) + '.pdf')
         $('.download').attr('download','cert' + (certID + 1) + '.pdf')
     });
     $('#left-click').click(function(self) {
@@ -31,7 +31,7 @@ $(document).ready(function () {
         if (certID > $('.flex-item').length - 1) certID = 0;
         $('.flex-item')[certID].style.display ='block';
         $('.cert:eq(' + certID + ')').attr('is_selected','true');
-        $('.download').attr('href','certificates/cert' + (certID + 1) + '.pdf')
+        $('.download').attr('href','/resources/cert' + (certID + 1) + '.pdf')
         $('.download').attr('download','cert' + (certID + 1) + '.pdf')
     });
     $('.cert').click(function() {
@@ -40,7 +40,7 @@ $(document).ready(function () {
         certID = parseInt(this.getAttribute('cert_id'))
         $('.flex-item')[certID].style.display ='block';
         $('.cert:eq(' + certID + ')').attr('is_selected','true');
-        $('.download').attr('href','certificates/cert' + (certID + 1) + '.pdf')
+        $('.download').attr('href','/resources/cert' + (certID + 1) + '.pdf')
         $('.download').attr('download','cert' + (certID + 1) + '.pdf')
     });
 
